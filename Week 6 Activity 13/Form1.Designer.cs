0fcf9178c00873691cb82c82854ef82ed3e59343
drajo <drajo@DESKTOP-LSTFBT7>
2021-10-28 11:03:18 -0700

    Add project files.

---
+++ Week 6 Activity 13/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace Week_6_Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ul = new System.Windows.Forms.TextBox();
+            this.um = new System.Windows.Forms.TextBox();
+            this.ur = new System.Windows.Forms.TextBox();
+            this.ml = new System.Windows.Forms.TextBox();
+            this.mr = new System.Windows.Forms.TextBox();
+            this.ll = new System.Windows.Forms.TextBox();
+            this.lr = new System.Windows.Forms.TextBox();
+            this.mm = new System.Windows.Forms.TextBox();
+            this.lm = new System.Windows.Forms.TextBox();
+            this.results = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ul
+            // 
+            this.ul.BackColor = System.Drawing.Color.White;
+            this.ul.Enabled = false;
+            this.ul.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ul.Location = new System.Drawing.Point(52, 31);
+            this.ul.Name = "ul";
+            this.ul.Size = new System.Drawing.Size(58, 62);
+            this.ul.TabIndex = 0;
+            this.ul.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // um
+            // 
+            this.um.Enabled = false;
+            this.um.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.um.Location = new System.Drawing.Point(128, 31);
+            this.um.Name = "um";
+            this.um.Size = new System.Drawing.Size(58, 62);
+            this.um.TabIndex = 0;
+            this.um.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ur
+            // 
+            this.ur.Enabled = false;
+            this.ur.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ur.Location = new System.Drawing.Point(204, 31);
+            this.ur.Name = "ur";
+            this.ur.Size = new System.Drawing.Size(58, 62);
+            this.ur.TabIndex = 0;
+            this.ur.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ml
+            // 
+            this.ml.Enabled = false;
+            this.ml.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ml.Location = new System.Drawing.Point(52, 99);
+            this.ml.Name = "ml";
+            this.ml.Size = new System.Drawing.Size(58, 62);
+            this.ml.TabIndex = 0;
+            this.ml.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // mr
+            // 
+            this.mr.Enabled = false;
+            this.mr.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mr.Location = new System.Drawing.Point(204, 99);
+            this.mr.Name = "mr";
+            this.mr.Size = new System.Drawing.Size(58, 62);
+            this.mr.TabIndex = 0;
+            this.mr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ll
+            // 
+            this.ll.Enabled = false;
+            this.ll.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ll.Location = new System.Drawing.Point(52, 167);
+            this.ll.Name = "ll";
+            this.ll.Size = new System.Drawing.Size(58, 62);
+            this.ll.TabIndex = 0;
+            this.ll.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lr
+            // 
+            this.lr.Enabled = false;
+            this.lr.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lr.Location = new System.Drawing.Point(204, 167);
+            this.lr.Name = "lr";
+            this.lr.Size = new System.Drawing.Size(58, 62);
+            this.lr.TabIndex = 0;
+            this.lr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // mm
+            // 
+            this.mm.Enabled = false;
+            this.mm.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mm.Location = new System.Drawing.Point(128, 99);
+            this.mm.Name = "mm";
+            this.mm.Size = new System.Drawing.Size(58, 62);
+            this.mm.TabIndex = 0;
+            this.mm.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lm
+            // 
+            this.lm.Enabled = false;
+            this.lm.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lm.Location = new System.Drawing.Point(128, 167);
+            this.lm.Name = "lm";
+            this.lm.Size = new System.Drawing.Size(58, 62);
+            this.lm.TabIndex = 0;
+            this.lm.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // results
+            // 
+            this.results.AcceptsReturn = true;
+            this.results.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.results.Enabled = false;
+            this.results.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.results.Location = new System.Drawing.Point(52, 235);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(209, 31);
+            this.results.TabIndex = 1;
+            this.results.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(109, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "New Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(314, 328);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.lr);
+            this.Controls.Add(this.mr);
+            this.Controls.Add(this.ur);
+            this.Controls.Add(this.ll);
+            this.Controls.Add(this.ml);
+            this.Controls.Add(this.lm);
+            this.Controls.Add(this.mm);
+            this.Controls.Add(this.um);
+            this.Controls.Add(this.ul);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ul;
+        private System.Windows.Forms.TextBox um;
+        private System.Windows.Forms.TextBox ur;
+        private System.Windows.Forms.TextBox ml;
+        private System.Windows.Forms.TextBox mr;
+        private System.Windows.Forms.TextBox ll;
+        private System.Windows.Forms.TextBox lr;
+        private System.Windows.Forms.TextBox mm;
+        private System.Windows.Forms.TextBox lm;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Week 6 Activity 13/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_6_Activity_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // list of each textbox used for storing game values
+            List<TextBox> display = new List<TextBox>();
+            display.Add(ul); display.Add(um); display.Add(ur); // top row
+            display.Add(ml); display.Add(mm); display.Add(mr); // middle row
+            display.Add(ll); display.Add(lm); display.Add(lr); // bottom row
+
+            // verify each textbox is empty at the start of every game
+            ClearTextBoxes(this.Controls);
+
+            Random r = new Random(); // generate random numbers for the game
+
+            // two-dimensional array for game board
+            int[,] board = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; // values are all unique to avoid game assuming winner at beginning
+            int boxCount = 0; // tracks which textbox is next
+
+            // two for-loops to iterate through the array
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = r.Next(0, 2);
+
+                    // display results on screen
+                    if (board[i, j] == 0)
+                    {
+                        display[boxCount].Text = "O";
+                    }
+                    else
+                    {
+                        display[boxCount].Text = "X";
+                    }
+                    
+                    boxCount++; // increments to use next textbox in list
+
+                    if (CheckResults(board) != "Draw")
+                    {
+                        goto GameEnd; // break out of both loops
+                    }
+                }
+            }
+
+            // print end results
+            GameEnd: results.Text = CheckResults(board).ToString();
+        }
+
+        public String CheckResults(int[,] board)
+        {
+            int winner = 9; // 0 and 1 are already assigned so 9 represents a draw
+
+            // win conditions
+            if (board[0, 0] == board[0, 1] && board[0, 0] == board[0, 2]) // across the top
+            {
+                winner = board[0,0];
+            }
+            else if (board[1, 0] == board[1, 1] && board[1, 0] == board[1, 2]) // across the middle
+            {
+                winner = board[1, 0];
+            }
+            else if (board[2, 0] == board[2, 1] && board[2, 0] == board[2, 2]) // across the bottom
+            {
+                winner = board[2, 0];
+            }
+            else if (board[0, 0] == board[1, 0] && board[0, 0] == board[2, 0]) // down the left
+            {
+                winner = board[0, 0];
+            }
+            else if (board[0, 1] == board[1, 1] && board[0, 1] == board[2, 1]) // down the middle
+            {
+                winner = board[0, 1];
+            }
+            else if (board[0, 2] == board[1, 2] && board[0, 2] == board[2, 2]) // down the right
+            {
+                winner = board[0, 2];
+            }
+            else if (board[0, 0] == board[1, 1] && board[0, 0] == board[2, 2]) // diagonal from the left to right
+            {
+                winner = board[0, 0];
+            }
+            else if (board[0, 2] == board[1, 1] && board[0, 2] == board[2, 0]) // diagonal from the right to left
+            {
+                winner = board[0, 2];
+            } 
+            else
+            {
+                winner = 9;
+            }
+
+            // return winner or draw
+            if (winner == 1)
+            {
+                return "X is the winner!";
+            }
+            else if (winner == 0)
+            {
+                return "O is the winner!";
+            }
+            else
+            {
+                return "Draw";
+            }
+        }
+
+        // Method clears all text boxes in control for each new game
+        // Credit: Thomas Daniels, 30-Aug-2014, https://www.codeproject.com/Questions/813344/Clear-all-textboxes-in-form-with-one-Function
+        public void ClearTextBoxes(Control.ControlCollection ctrlCollection)
+        {
+            foreach (Control ctrl in ctrlCollection)
+            {
+                if (ctrl is TextBoxBase)
+                {
+                    ctrl.Text = String.Empty;
+                }
+                else
+                {
+                    ClearTextBoxes(ctrl.Controls);
+                }
+            }
+        }
+    }
+}
